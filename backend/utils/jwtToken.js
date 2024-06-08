cb35ad1cef8b23f5d4e977e9afeccc9191a6a0ef
kaustubh <kaustubhsharma1601@gmail.com>
2024-06-09 00:18:11 +0530

    check

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -2,7 +2,7 @@
     const token = user.generateJsonWebToken()
     const cookieName = user.role === "Admin" ? "adminToken" : "patientToken";
     res.status(statusCode).cookie(cookieName, token, {
-        httpOnly:false,
+        secure:true,
         expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000)
     })
     .json({
