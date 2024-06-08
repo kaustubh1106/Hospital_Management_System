217bfeb044446c893950ca9f19917b6a11ea202f
kaustubh <kaustubhsharma1601@gmail.com>
2024-06-08 18:25:57 +0530

    changed deployed site doamin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:6036/api/v1/user/patient/me",
+          "https://hospital-management-system-tau-weld.vercel.app/api/v1/user/patient/me",
           {
             withCredentials: true,
           }
